181b4eea7909e3d56b5b5f93af9bf2a7497f140f
Jayla Nickens <jaylanickensgill11@gmail.com>
2020-07-28 16:23:28 +0000

    .

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
-def my_function(i)
+#code sourced from http://pythonfiddle.com/nim-game-prototype/ and edited
 
-return 2* i
-print(my_function(3))
-print 
+
+total =0
+sticks = (13)
+
+playerState = into(0)
+
+#game mechancis 
+
+
+while (sticks > 0):
+   if (playerState %2 !=1):
+      playerMove = int(raw_inut("Zoey,Please enter your move:"))
+    if (playerMove > 0) and (playerMove < 5) and (playerMove <= sticks):
+    		sticks = sticks - playerMove
+      playerState +=1
+      print sticks 
+      print playerState
+     else: 
+        print "Wrong move, try again"
+       else: 
+          playerMove = int(raw_inut("Josh,Please enter your move:"))
+             if (playerMove > 0) and (playerMove < 5) and (playerMove <= sticks):
+      sticks = sticks - playerMove
+      playerState +=1
+      print sticks 
+      print playerState
+
+winner = int(playerState %2)
+
+if (winner ! =1)
+print ("Congrats Zoey!")
+else:
+  print ("Congrats Josh!")
+
+    
